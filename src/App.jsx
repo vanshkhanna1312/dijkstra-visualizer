9aa40a29f4a36b2033ff4b3240c179a9cd2bbe7b
vanshkhanna1312 <99236911+vanshkhanna1312@users.noreply.github.com>
2024-06-22 00:08:59 +0530

    new files

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PathfindingVisualizer from "./PathfindingVisualizer/PathfindingVisualizer";
+
+function App() {
+  return <PathfindingVisualizer />;
+}
+
+export default App;
